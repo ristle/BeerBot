92e287116ea5dd29be51e68f2fb23e990c6b5093
Ristleell <ristle@yandex.ru>
2020-07-26 21:50:32 +0300

    Fixed send list from /main

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ import json
 NAME = None
 TOKEN = None
 LAST_REPLY_MESSAGE = None
-ADD_PERSON=False
+ADD_PERSON = False
 TIMEZONE = 'Europe/Moscow'
 TIMEZONE_COMMON_NAME = 'Moscow'
 
@@ -15,11 +15,13 @@ girls = ['Тома', 'Соня']
 with open('key.txt', 'r') as file:
     TOKEN = file.read()
 
+
 def load_beer_list():
     data = None
     with open('../data/beer.json', encoding='utf-8') as fh:
         data = json.load(fh)
     return data
+
 
 def save_beer_list(js):
     with open("../data/beer.json", 'w', encoding='utf8') as json_file:
